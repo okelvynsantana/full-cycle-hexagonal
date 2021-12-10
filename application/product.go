8659202cfabf8377a25b091ecfd909c0cb346357
Kelvyn Santana <santanakelvyn@gmail.com>
2021-12-09 23:08:22 -0300

    add product service interface and implementation

--- application/product.go
+++ application/product.go
@@ -1,7 +1,10 @@
 package application
+
 import (
 	"errors"
+
 	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
 )
 
 type ProductInterface interface {
@@ -14,6 +17,33 @@ type ProductInterface interface {
 	GetPrice() float64
 }
 
+type ProductServiceInterface interface {
+	Get(id string) (ProductInterface, error)
+	Create(name string, price float64) (ProductInterface, error)
+	Enable(product ProductInterface) (ProductInterface, error)
+	Disable(product ProductInterface) (ProductInterface, error)
+}
+
+type ProductReader interface {
+	Get(id string) (ProductInterface, error)
+}
+
+type ProductWriter interface {
+	Save(product ProductInterface) (ProductInterface, error)
+}
+
+type ProductPersistenceInterface interface {
+	ProductReader
+	ProductWriter
+}
+
+type Product struct {
+	ID string `valid:"uuidv4"`
+	Name string `valid:"required"`
+	Price float64 `valid:"float, optional"`
+	Status string`valid:"required"`
+}
+
 const (
 	DISABLED = "disabled"
 	ENABLED = "enabled"
@@ -23,11 +53,14 @@ func init() {
 	govalidator.SetFieldsRequiredByDefault(true)
 }
 
-type Product struct {
-	ID string `valid:"uuidv4"`
-	Name string `valid:"required"`
-	Price float64 `valid:"float, optional"`
-	Status string`valid:"required"`
+
+func NewProduct() *Product {
+	product := Product {
+		ID: uuid.NewV4().String(),
+		Status: DISABLED,
+	}
+
+	return &product
 }
 
 func(p *Product) IsValid() (bool, error) {
---
+++ application/product_service_test.go
@@ -0,0 +1,2 @@
+package application_test
+
